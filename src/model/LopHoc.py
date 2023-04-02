3b01127882afc186af74fc51c6f8255312f35fcb
ldkhoi <ldkhoi@ctu.edu.vn>
2023-04-02 09:02:16 +0700

    model lop

--- src/model/LopHoc.py
+++ src/model/LopHoc.py
@@ -1,29 +1,47 @@
-import psycopg2
-import dataprovider as dp
+from database import db
 
-class LopHoc:
-    def __init__(self, lop_maso=None, lop_ten=None, lop_soluong_sv=None, lop_convan=None, nh_id=None, nienkhoa=None):
-        self.lop_id = None
-        self.lop_maso = lop_maso
-        self.lop_ten = lop_ten
-        self.lop_soluong_sv = lop_soluong_sv
-        self.lop_convan = lop_convan
-        self.nh_id = nh_id
-        self.nienkhoa = nienkhoa
+class LopHoc(db.Model):
+    __tablename__ = 'lophoc'
+    lop_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lop_maso = db.Column(db.String(20), nullable=False)
+    lop_ten = db.Column(db.Text, nullable=False)
+    lop_soluong_sv = db.Column(db.Integer, nullable=False)
+    lop_convan = db.Column(db.Integer, nullable=False)
+    nh_id = db.Column(db.Integer, nullable=False)
+    nienkhoa = db.Column(db.Integer, nullable=False)
+    
+    @staticmethod
+    def get_lop():
+        lophocs = LopHoc.query.order_by(LopHoc.lop_id.desc()).all()
+        return lophocs
+
+    @staticmethod
+    def save_lophoc(lop_id, lop_maso, lop_ten, lop_soluong_sv, lop_convan, nh_id, nienkhoa):
+        print("---------- Saving lophoc -------------")
+        existing_lophoc = LopHoc.query.filter_by(lop_maso=lop_maso).first()
         
-        
-    def get_lophoc_list(self, cbmaso):
-        conn = dp.connect()
-        cur = conn.cursor()
-        try:
-            cur.execute("SELECT distinct lh.* FROM public.lophoc lh \
+        # Nếu tồn tại lophoc khác có cùng maso, trả về thông báo lỗi
+        if existing_lophoc and (str(existing_lophoc.lop_id) != str(lop_id)):
+            return None
+        else:
+            print("có thể saved")
+            lophoc = LopHoc.query.filter_by(lop_id = lop_id).first() or LopHoc()
+            lophoc.lop_id = lop_id
+            lophoc.lop_maso = lop_maso
+            lophoc.lop_ten = lop_ten
+            lophoc.lop_soluong_sv = lop_soluong_sv
+            lophoc.lop_convan = lop_convan
+            lophoc.nh_id = nh_id
+            lophoc.nienkhoa = nienkhoa
+            db.session.add(lophoc)  
+            db.session.commit()
+
+
+    def get_lophoc_list(cbmaso):
+            query = "SELECT distinct lh.* FROM public.lophoc lh \
                 INNER JOIN thoikhoabieu tkb USING (lop_id) \
                 INNER JOIN canbo cb ON tkb.cb_id = cb.cb_id \
-                WHERE cb_maso = %s", (cbmaso, ))
-            rows = cur.fetchall()
-        except psycopg2.Error as e:
-            print("Error selecting rows: ", e)
-        finally:
-            cur.close()
-            conn.close()
-        return rows
+                WHERE cb_maso = :cb_maso"
+            params = {'cb_maso': cbmaso}
+            result = db.session.execute(query, params)
+            return result
